edb9106978d1785be86dee3c7c63e04088279f83
Ben Gibson <brgibson@indiana.edu>
2015-05-17 13:27:38 -0700

    blog - adding cross origin headers support

--- core/server/index.js
+++ core/server/index.js
@@ -197,6 +197,26 @@ function init(options) {
             blogApp.use(compress());
         }
 
+        // Add headers
+        blogApp.use(function (req, res, next) {
+
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', '*');
+
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', true);
+
+            // Pass to next layer of middleware
+            next();
+        });
+
         // ## View engine
         // set the view engine
         blogApp.set('view engine', 'hbs');
